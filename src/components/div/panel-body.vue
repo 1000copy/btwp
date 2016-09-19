4704cf8a6ab27ed194189b3aba1b00961789eb07
1000copy <1000copy@gmail.com>
2016-09-19 18:32:03 +0800

    div and span NEW Catalog

---
+++ src/components/div/panel-body.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="panel-body">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/div/panel.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="panel" :class="cc">
+      <slot name="header"></slot>
+      <slot name="body"></slot>
+      <slot name="footer"></slot>
+      <!-- <slot ></slot> -->
+  </div>
+</template>
+
+<script>
+export default {
+ props: {
+    type: {
+      type: String,
+      require: true
+    },
+    closeable: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    close: function (event) { 
+      this.visible = false
+    }
+  },
+  data () {
+    return {
+      cc:{
+        'panel-default': this.type=='default',
+        'panel-primary': this.type=='primary',
+        'panel-success': this.type=='success',
+        'panel-info': this.type=='info',
+        'panel-warning': this.type=='warning',
+        'panel-danger': this.type=='danger'
+      },
+      header:true,
+      footer:true
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
