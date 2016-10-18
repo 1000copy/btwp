c718f4a65e4480be8a3be40552662458bb84f0df
1000copy <1000copy@gmail.com>
2016-10-18 16:13:31 +0800

    docs+

--- src/App.vue
+++ src/App.vue
@@ -1,114 +1,152 @@
 <template>
   <div id="app">
-  <h1>基础类</h1>
-  <h2>list</h2>
-  <list-doc></list-doc>
-    <breadcrumb>
-        <li><a href="#">Level 1</a></li>
-        <li><a href="#">Level 2</a></li>
-        <li class="active">Level 3</li>
-    </breadcrumb>
-    <pagination size="large">
-        <li class="disabled"><a href="#">&laquo;</a></li>
-        <li class="active"><a href="#">1</a></li>
-        <li><a href="#">2</a></li>
-        <li><a href="#">3</a></li>
-        <li><a href="#">&raquo;</a></li>
-    </pagination>
-    <pager>
-      <pager-part type="prev"><a href="#">Previous</a></pager-part>
-      <pager-part type="next"><a href="#">Next</a></pager-part>
-    </pager>
+    <container>
+      <row>
+        <column type="md-12">
+           <navbar>
+              <navbar-part type="logo" href="#logo">
+                <h1>BootView docs</h1>
+              </navbar-part>
+            </navbar>
+        </column>
+      </row>
+      <row>
+        <column  type="md-4 sm-4">
+            <panel type="info">
+              <part type="header">基础类</part>
+              <part type="body">
+                 <list type="group">
+                   <lp context="vested"><a href="#layout">ul</a></lp>
+                   <lp context="vested"><a href="#span">span</a></lp>
+                   <lp type="vested">image</lp>
+                   <lp type="vested">button</lp>
+                   <lp type="vested">input</lp>
+                   <lp type="vested">form</lp>
+                 </list>
+              </part>
+            </panel>
+            <panel type="info">
+              <part type="header">高级类</part>
+              <part type="body">
+                 <list type="group">
+                   <lp context="vested">dropdown</lp>
+                   <lp context="vested">jumbotrom</lp>
+                   <lp type="vested">carousel</lp>
+                   <lp type="vested">navbar</lp>
+                   <lp type="vested">popup</lp>
+                 </list>
+              </part>
+            </panel>
+            <panel type="info">
+              <part type="header">布局</part>
+              <part type="body">
+                 <list type="group">
+                   <lp context="vested">布局</lp>
+                 </list>
+              </part>
+            </panel>
+        </column>
+        <column  type="md-8 sm-4"  >
+            <h1 id="list">list</h1>
+            <list-doc ></list-doc> 
+            <h1 id="span">span</h1>      
+            <span-doc></span-doc>
+           
+           <image1 type="round" src="http://placehold.it/100X100"></image1>    
+            <btn type="primary" size="large">Run</btn>
+            <btn type="primary" size="small">Run</btn>
+            <btn type="primary" size="xsmall">Run</btn>
+            <tooltip  direction="right" href="#">tooltip</tooltip>
+            <alert type="success">success</alert>
+            <btn type="info">btn</btn>
+            
+            
 
-    <btn type="primary" size="large">Run</btn>
-    <btn type="primary" size="small">Run</btn>
-    <btn type="primary" size="xsmall">Run</btn>
-    <tooltip  direction="right" href="#">tooltip</tooltip>
-    <alert type="success">success</alert>
-    <icon type="home"></icon>
-    <btn type="info">btn</btn>
-    <lbl type="info">label</lbl>
-
-   <panel type="info">
-      <part type="header">header</part>
-      <part type="body">body</part>
-      <part type="footer">footer</part>
-    </panel>
-    <well size="large">well</well>
-    <list type="inline">
-      <lp type="naked" >Item1</lp>
-      <lp type="vested">Item2</lp>
-      <lp type="vested">Item3</lp>
-    </list>
-   
-    <nv type="pill">
-        <li class="active"><a href="#">Home</a></li>
-        <li><a href="#">Login</a></li>
-        <li><a href="#">About</a></li>
-    </nv>
-    <nv type="tab">
-        <li class="active"><a href="#">Home</a></li>
-        <li><a href="#">Login</a></li>
-        <li><a href="#">About</a></li>
-    </nv>
-    <dropdown>
-        <li><a href="#">Action1</a></li>
-        <li><a href="#">Action2</a></li>
-    </dropdown>
-    <btn type="info">
-        <badge>42</badge>
-    </btn>
-    <!-- navbar -->
-    <navbar>
-      <navbar-part type="logo" href="#logo">Logo</navbar-part>
-      <navbar-part type="nav" >
-        <li><a href="#">Link 1</a></li>
-        <li><a href="#">Link 2</a></li>
-      </navbar-part>
-      <navbar-part type="dropdown" >
-          <li><a href="#">Action1</a></li>
-          <li><a href="#">Action2</a></li>
-      </navbar-part>
-      <navbar-part type="search"></navbar-part>
-    </navbar>
-    <!-- carousel -->
-    <carousel>
-       <carousel-part type="indicators">
-            <carousel-item type="indicator" :index="0" active></carousel-item>
-            <carousel-item type="indicator" :index="1" ></carousel-item>
-            <carousel-item type="indicator" :index="2" ></carousel-item>
-        </carousel-part>
-        <!-- Wrapper for carousel items -->
-        <carousel-part type="inner">
-            <carousel-item  src="http://placehold.it/600X50" active></carousel-item>
-            <carousel-item  src="http://placehold.it/600X50" ></carousel-item>
-            <carousel-item  src="http://placehold.it/600X50" ></carousel-item>
-        </carousel-part>
-    </carousel>
-    <image1 type="round" src="http://placehold.it/600X50"></image1>
-    <div>
-      <btn type="primary" data-toggle="modal" data-target="#myModal">modal</btn>
-      <modal id="myModal">
-        <span slot="title">titlespan</span>
-        <h1 slot="body">pbody</h1>
-      </modal>
-    <div>
-    <!-- 问题不少，先放着 -->
-    <sel1 type="multi"></sel1>
-    <!-- <sel1 type="single"></sel1> -->
-    <jumbotron>
-      <h1>title</h1>
-      <p>content</p>
-      <p><btn type="primary" size="large">Run</a></p>
-    </jumbotron>
-    <frm type="inline">
-        <form-control label="Email" type="email"></form-control>
-        <form-control label="Password" type="password"></form-control>
-        <btn type="primary">Login</button>
-    </frm>
-   
-  </div>
-  <container>
+            <panel type="info">
+              <part type="header">header</part>
+              <part type="body">body</part>
+              <part type="footer">footer</part>
+            </panel>
+            <well size="large">well</well>
+            <list type="inline">
+              <lp type="naked" >Item1</lp>
+              <lp type="vested">Item2</lp>
+              <lp type="vested">Item3</lp>
+            </list>
+           
+            <nv type="pill">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#">Login</a></li>
+                <li><a href="#">About</a></li>
+            </nv>
+            <nv type="tab">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#">Login</a></li>
+                <li><a href="#">About</a></li>
+            </nv>
+            <dropdown>
+                <li><a href="#">Action1</a></li>
+                <li><a href="#">Action2</a></li>
+            </dropdown>
+            <btn type="info">
+                <badge>42</badge>
+            </btn>
+            <!-- navbar -->
+            <navbar>
+              <navbar-part type="logo" href="#logo">Logo</navbar-part>
+              <navbar-part type="nav" >
+                <li><a href="#">Link 1</a></li>
+                <li><a href="#">Link 2</a></li>
+              </navbar-part>
+              <navbar-part type="dropdown" >
+                  <li><a href="#">Action1</a></li>
+                  <li><a href="#">Action2</a></li>
+              </navbar-part>
+              <navbar-part type="search"></navbar-part>
+            </navbar>
+            <!-- carousel -->
+            <carousel>
+               <carousel-part type="indicators">
+                    <carousel-item type="indicator" :index="0" active></carousel-item>
+                    <carousel-item type="indicator" :index="1" ></carousel-item>
+                    <carousel-item type="indicator" :index="2" ></carousel-item>
+                </carousel-part>
+                <!-- Wrapper for carousel items -->
+                <carousel-part type="inner">
+                    <carousel-item  src="http://placehold.it/600X50" active></carousel-item>
+                    <carousel-item  src="http://placehold.it/600X50" ></carousel-item>
+                    <carousel-item  src="http://placehold.it/600X50" ></carousel-item>
+                </carousel-part>
+            </carousel>
+            
+            <div>
+              <btn type="primary" data-toggle="modal" data-target="#myModal">modal</btn>
+              <modal id="myModal">
+                <span slot="title">titlespan</span>
+                <h1 slot="body">pbody</h1>
+              </modal>
+            <div>
+            <!-- 问题不少，先放着 -->
+            <sel1 type="multi"></sel1>
+            <!-- <sel1 type="single"></sel1> -->
+            <jumbotron>
+              <h1>title</h1>
+              <p>content</p>
+              <p><btn type="primary" size="large">Run</a></p>
+            </jumbotron>
+            <frm type="inline">
+                <form-control label="Email" type="email"></form-control>
+                <form-control label="Password" type="password"></form-control>
+                <btn type="primary">Login</button>
+            </frm>
+           
+          </div>
+        </column>
+      </row>
+  </container>
+  
+    
+  <container id="layout">
         <row>
           <column  type="md-1 sm-4" style="background-color:blue">
               left
@@ -125,6 +163,7 @@
 
 <script>
 import listDoc from './docs/list'
+import spanDoc from './docs/span'
 import Hello from './components/Hello'
 import alert from './components/div/alert'
 import panel from './components/div/panel'
@@ -136,12 +175,7 @@ import icon from './components/span/icon'
 import badge from './components/span/badge'
 import btn from './components/btn'
 import lbl from './components/span/label'
-import list from './components/ul/list'
-import lp from './components/ul/list-part'
-import breadcrumb from './components/ul/breadcrumb'
-import pagination from './components/ul/pagination'
-import pager from './components/ul/pager'
-import pagerPart from './components/ul/pager-part'
+
 import nv from './components/nav/nav'
 import navbar from './components/nav/navbar'
 import navbarPart from './components/nav/navbar-part'
@@ -157,9 +191,17 @@ import formControl from './components/advance/form-control'
 import container from './components/layout/container'
 import row from './components/layout/row'
 import column from './components/layout/column'
+// ul 
+import list from './components/ul/list'
+import lp from './components/ul/list-part'
+import breadcrumb from './components/ul/breadcrumb'
+import pagination from './components/ul/pagination'
+import pager from './components/ul/pager'
+import pagerPart from './components/ul/pager-part'
+
 export default {
   components: {    
-    listDoc,
+    listDoc,spanDoc,
     alert,
     icon,badge,
     btn,lbl,
@@ -198,6 +240,8 @@ pre {
     white-space: pre-wrap;
     background: hsl(30,80%,90%);
 }
-
+h1{
+  background: hsl(30,80%,90%);
+}
 
 </style>
--- src/docs/list.vue
+++ src/docs/list.vue
@@ -1,5 +1,5 @@
 <template>
- <h3>group</h2>
+ <h2 >group</h2>
   <p>分组是对ul标签的css改造。可以看案例：</p>
   <h4>外形</h4>
    <list type="inline">
@@ -8,7 +8,7 @@
       <lp type="vested">Item3</lp>
       <lp type="vested">Item4</lp>
     </list>
-  <h4>代码</h4>
+  <h3>代码</h3>
     <xmp>
       <list type="inline">
         <lp context="success">Item1</lp>
@@ -30,4 +30,67 @@
       <tr><td>type</td><td>字符串</td><td>vested|naked</td><td>指示列表项是否套线</td></tr>
       <tr><td></td><td></td><td></td></tr>
     </table>
+    <h2 id="breadcrumb">breadcrumb</h2>
+    <p>breadcrumb是对ul标签的css改造。可以显示当前页面的导航位置。看案例：</p>
+    <!-- breadcrumb -->
+    <breadcrumb>
+        <li><a href="#">Level 1</a></li>
+        <li><a href="#">Level 2</a></li>
+        <li class="active">Level 3</li>
+    </breadcrumb>
+    <h3>代码</h3>
+    <xmp>
+         <breadcrumb>
+            <li><a href="#">Level 1</a></li>
+            <li><a href="#">Level 2</a></li>
+            <li class="active">Level 3</li>
+        </breadcrumb>
+      </xmp>
+    <!-- pagination -->
+     <h2 id="pagination">group</h2>
+    <p>pagination是对ul标签的css改造。可以显示分页列表和当前分页。看案例：</p>
+    <pagination size="large">
+        <li class="disabled"><a href="#">&laquo;</a></li>
+        <li class="active"><a href="#">1</a></li>
+        <li><a href="#">2</a></li>
+        <li><a href="#">3</a></li>
+        <li><a href="#">&raquo;</a></li>
+    </pagination>
+    <h3>代码</h3>
+    <xmp>
+        <pagination size="large">
+        <li class="disabled"><a href="#">&laquo;</a></li>
+        <li class="active"><a href="#">1</a></li>
+        <li><a href="#">2</a></li>
+        <li><a href="#">3</a></li>
+        <li><a href="#">&raquo;</a></li>
+    </pagination>
+    </xmp>
+    <!-- pager -->
+     <h2 id="pager">pager</h2>
+     <p>pager是对ul标签的css改造。可以显示翻页按钮，向前或者向后翻页。看案例：</p>
+    <pager>
+      <pager-part type="prev"><a href="#">Previous</a></pager-part>
+      <pager-part type="next"><a href="#">Next</a></pager-part>
+    </pager>
+    <h3>代码</h3>
+    <xmp>
+         <pager>
+            <pager-part type="prev"><a href="#">Previous</a></pager-part>
+            <pager-part type="next"><a href="#">Next</a></pager-part>
+          </pager>
+    </xmp>
 </template>
+<script>
+  import list from '../components/ul/list'
+  import lp from '../components/ul/list-part'
+  import breadcrumb from '../components/ul/breadcrumb'
+  import pagination from '../components/ul/pagination'
+  import pager from '../components/ul/pager'
+  import pagerPart from '../components/ul/pager-part'
+  export default {
+    components: {    
+      list,lp,breadcrumb,pagination,pager,pagerPart
+    }
+  }
+</script>
